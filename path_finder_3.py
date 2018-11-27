152a93900dee9a878bd6b31a88313dc97739b120
Adam Tegtmeier <ategtmei@gmail.com>
2018-11-26 22:18:57 -0500

    Minor comment cleanup.

--- path_finder_3.py
+++ path_finder_3.py
@@ -19,19 +19,12 @@ def rsolve(grid, grid_keys, x, y, end_t, previous_altitude, previous_best, big_v
                 rsolve(grid, grid_keys, x, y-1, end_t, cval, best_candidate, big_val, best_distance)
 
 def path_finder(area):
-#     if len(area) > 300:
-#         print(area, len(area))
-#         return 95
-#         #raise Exception('')
-#
     big_val = float('inf')
     best_distance = defaultdict(lambda:big_val)
 
     rows = area.split("\n")
     end_t = (len(rows[0]) - 1, len(rows) - 1)
-    grid = dict({((x, y), int(c)) for y, r in enumerate(area.split("\n")) for x, c in enumerate(r)})
-    
-    
-    
+    grid = dict({((x, y), int(c)) for y, r in enumerate(area.split("\n")) for x, c in enumerate(r)})    
+
     rsolve(grid, grid.keys(), 0, 0, end_t, grid[(0, 0)], 0, big_val, best_distance)  # total levels climbed
     return best_distance[end_t]
